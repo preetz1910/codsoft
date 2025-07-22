27c687b64ed10ae181f1a73019ea8adfb093b079
preetz1910 <preetha.240231@aids.ritchennai.edu.in>
2025-07-22 13:46:16 +0530

    Add files via upload

---
+++ land/script.js
@@ -0,0 +1,13 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 70,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
