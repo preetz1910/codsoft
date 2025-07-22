edef803e2ca8d83801089302b5d6b89115a14dad
preetz1910 <preetha.240231@aids.ritchennai.edu.in>
2025-07-22 13:38:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-// You can add interactivity here later
-console.log("Portfolio loaded successfully.");
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 70,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
